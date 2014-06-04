97797e01980ef2c3735a15c73a2de7c099aae476
Helen Hood <helen.c.hood@gmail.com>
2014-06-04 14:52:52 -0400

    Index page tests passing

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,2 +1,30 @@
 class Recipe
+  attr_reader :id, :name
+
+  def initialize(id, name, instructions = nil , description = nil)
+    @id = id
+    @name = name
+    @instructions = instructions
+    @description = description
+  end
+
+  def self.all
+    query = "SELECT * FROM recipes"
+
+    connection = PG.connect(dbname: 'recipes')
+    results = connection.exec(query)
+    connection.close
+
+    recipes = []
+    results.each do |result|
+      recipes << Recipe.new(
+        result['id'],
+        result['name'],
+        result['instructions'],
+        result['description']
+        )
+    end
+
+    recipes
+  end
 end
