295bef0dfce1d556fc1b3ea0ab366a6a6d735e3d
Helen Hood <helen.c.hood@gmail.com>
2014-06-05 16:25:08 -0400

    Find method retrieves recipe

--- models/recipe.rb
+++ models/recipe.rb
@@ -1,11 +1,12 @@
 class Recipe
-  attr_reader :id, :name, :description, :instructions
+  attr_reader :id, :name, :description, :instructions, :ingredients
 
-  def initialize(id, name, instructions = nil , description = nil)
+  def initialize(id, name, instructions = nil, description = nil, ingredients = [])
     @id = id
     @name = name
     @instructions = instructions
     @description = description
+    @ingredients = ingredients
   end
 
   def self.all
@@ -29,17 +30,20 @@ class Recipe
   end
 
   def self.find(id)
-    query = "SELECT * FROM recipes WHERE id = $1"
+    recipe_query = "SELECT * FROM recipes WHERE id = $1"
+    ingred_query = "SELECT * FROM ingredients WHERE recipe_id = $1"
 
     connection = PG.connect(dbname: 'recipes')
-    result = connection.exec_params(query, [id]).first
+    recipe_result = connection.exec_params(recipe_query, [id]).first
+    ingredients_result = connection.exec_params(ingred_query, [id]).to_a
     connection.close
 
     Recipe.new(
-      result['id'],
-      result['name'],
-      result['instructions'],
-      result['description']
+      recipe_result['id'],
+      recipe_result['name'],
+      recipe_result['instructions'] || "This recipe doesn't have any instructions.",
+      recipe_result['description'] || "This recipe doesn't have a description.",
+      ingredients
       )
   end
 end
